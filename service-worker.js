714e38b00ca2ad50e22dc903849dfc440975ac0c
Diana Weinhold <d.weinhold@lse.ac.uk>
2025-03-29 18:13:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'conservation-game-v1.14.3'; // or whatever version you prefer
+const CACHE_NAME = 'conservation-game-v1.14.6'; // or whatever version you prefer
 
 const urlsToCache = [
   '/',
